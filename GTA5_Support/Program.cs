565e4e05b4c0ce0ab89b734690e345ee75a05e03
Hadagalberto Junior <hadagalberto.junior1@gmail.com>
2021-07-20 03:19:00 -0300

    Added support to disable adaptative triggers

--- GTA5_Support/Program.cs
+++ GTA5_Support/Program.cs
@@ -2,6 +2,7 @@
 using DualSenseSupport;
 using System;
 using System.Threading;
+using System.Windows.Input;
 
 namespace GTA5_Support
 {
@@ -11,8 +12,15 @@ namespace GTA5_Support
         static DsTrigger.Modes weaponMode = DsTrigger.Modes.Pulse_AB;
         static DsTrigger.Modes offMode = DsTrigger.Modes.Off;
 
+        static bool active = true;
+        static bool isRunning = true;
+
         static void Main(string[] args)
         {
+            Thread thread = new Thread(KeyboardThread);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
             Devices.Init();
             Console.Clear();
             if (Devices.GetDeviceCount() == 0)
@@ -32,70 +40,56 @@ namespace GTA5_Support
             Console.WriteLine("Program started, try some weapons");
 
             var selectedDevice = 0;
-            
 
             while (true)
             {
-                var fireRate = MemoryReader.GetFireRate();
-
-                double forceDouble = (fireRate * 16) / 72;
-                int force = (int)Math.Round(fireRate < 50 ? forceDouble / 3 : forceDouble, 0);
-                Devices.GetDevice(selectedDevice).SetTriggerRight(new DsTrigger
+                if (active)
                 {
-                    Force1 = 255,
-                    Force2 = 10,
-                    Force3 = 0,
-                    Force4 = 0,
-                    Force5 = 0,
-                    Force6 = 255,
-                    Force7 = force,
-                    Mode = weaponMode
-                });
+                    var fireRate = MemoryReader.GetFireRate();
+
+                    double forceDouble = (fireRate * 16) / 72;
+                    int force = (int)Math.Round(fireRate < 50 ? forceDouble / 3 : forceDouble, 0);
+                    Devices.GetDevice(selectedDevice).SetTriggerRight(new DsTrigger
+                    {
+                        Force1 = 90,
+                        Force2 = 10,
+                        Force3 = 0,
+                        Force4 = 0,
+                        Force5 = 0,
+                        Force6 = 255,
+                        Force7 = force,
+                        Mode = weaponMode
+                    });
+                }
+                else
+                {
+                    Devices.GetDevice(selectedDevice).SetTriggerRight(new DsTrigger
+                    {
+                        Force1 = 0,
+                        Force2 = 0,
+                        Force3 = 0,
+                        Force4 = 0,
+                        Force5 = 0,
+                        Force6 = 0,
+                        Force7 = 0,
+                        Mode = offMode
+                    });
+                }
                 Thread.Sleep(100);
-                //if (MemoryReader.GetFireRate() == 55)
-                //{
-                //    Devices.GetDevice(selectedDevice).SetTriggerRight(new DsTrigger
-                //    {
-                //        Force1 = 255,
-                //        Force2 = 10,
-                //        Force3 = 0,
-                //        Force4 = 0,
-                //        Force5 = 0,
-                //        Force6 = 255,
-                //        Force7 = 12,
-                //        Mode = weaponMode
-                //    });
-                //}
-                //if (MemoryReader.GetFireRate() == 100)
-                //{
-                //    Devices.GetDevice(selectedDevice).SetTriggerRight(new DsTrigger
-                //    {
-                //        Force1 = 255,
-                //        Force2 = 10,
-                //        Force3 = 0,
-                //        Force4 = 0,
-                //        Force5 = 0,
-                //        Force6 = 255,
-                //        Force7 = 22,
-                //        Mode = weaponMode
-                //    });
-                //}
-                //else
-                //{
-                //    Devices.GetDevice(selectedDevice).SetTriggerRight(new DsTrigger
-                //    {
-                //        Force1 = 0,
-                //        Force2 = 0,
-                //        Force3 = 0,
-                //        Force4 = 0,
-                //        Force5 = 0,
-                //        Force6 = 0,
-                //        Force7 = 0,
-                //        Mode = offMode
-                //    });
-                //}
             }
 
+        }
+
+        static void KeyboardThread()
+        {
+            while (isRunning)
+            {
+                Thread.Sleep(100);
+                if((Keyboard.GetKeyStates(Key.F7) & KeyStates.Down) > 0)
+                {
+                    active = !active;
+                }
+            }
         }
     }
 }
